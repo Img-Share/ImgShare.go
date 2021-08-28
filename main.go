c00b4ea8707b5b8ddd3dfb405d14c5448543966e
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-08-28 12:00:49 -0400

    do the thing

--- main.go
+++ main.go
@@ -4,4 +4,12 @@ import "github.com/bwmarrin/discordgo"
 
 func main() {
 	discord, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		panic(err)
+	}
+	err = discord.Open()
+	if err != nil {
+		panic(err)
+	}
+	discord.Close()
 }
