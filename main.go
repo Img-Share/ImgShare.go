314147e2b4779cfa0c14b9d5fa81ef53c569dcf6
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-08-28 17:59:04 -0400

    Add basic commands functionality

--- main.go
+++ main.go
@@ -1,15 +1,54 @@
 package main
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var commands map[string]interface{}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	iscmd := strings.HasPrefix(m.Content, "#")
+	if iscmd {
+		// It's a command
+		if val, ok := commands[m.Content[1:]]; ok {
+			val.(func(*discordgo.Session, *discordgo.MessageCreate))(s, m)
+		} else {
+			s.ChannelMessageSend(m.ChannelID, "Unknown command.")
+		}
+	}
+}
+
+func ping(s *discordgo.Session, m *discordgo.MessageCreate) {
+	s.ChannelMessageSend(m.ChannelID, "pong")
+}
 
 func main() {
 	discord, err := discordgo.New("Bot " + Token)
+	commands = make(map[string]interface{})
+	commands["ping"] = ping
+
 	if err != nil {
 		panic(err)
 	}
+
+	discord.AddHandler(messageCreate)
+
 	err = discord.Open()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Now running bot. Press CTRL+C to quit")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
 	discord.Close()
 }
