86e9ca05143b4896824cca4ce8d0f9eaa206e0d1
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-08-29 20:54:32 -0400

    after hours of banging my head on my desk, it works

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"runtime"
 	"strings"
 	"syscall"
 
@@ -39,10 +40,8 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	iscmd := strings.HasPrefix(m.Content, "#")
 	if iscmd {
 		// It's a command
-		if val, ok := commands[m.Content[1:]]; ok {
-			val.(func(*discordgo.Session, *discordgo.MessageCreate))(s, m)
-		} else {
-			s.ChannelMessageSend(m.ChannelID, "Unknown command.")
+		if val, ok := commands[strings.Split(m.Content, " ")[0][1:]]; ok {
+			val.(func(*discordgo.Session, *discordgo.MessageCreate, []string))(s, m, strings.Split(m.Content, " ")[1:])
 		}
 	}
 }
@@ -52,17 +51,30 @@ func ping(s *discordgo.Session, m *discordgo.MessageCreate) {
 	println(m.Attachments[0].ProxyURL)
 }
 
-func postmeme(s *discordgo.Session, m *discordgo.MessageCreate) {
+func postmeme(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
 	if len(m.Attachments) < 1 {
 		s.ChannelMessageSend(m.ChannelID, "No image found.")
 		return
 	}
-	go saveFile("memes/"+m.Attachments[0].Filename, m.Attachments[0].ProxyURL)
+	ext := m.Attachments[0].Filename[strings.LastIndex(m.Attachments[0].Filename, "."):]
+	fn := m.Attachments[0].Filename
+	println(args)
+	if len(args) > 0 {
+		fn = args[0] + ext
+		println(fn)
+	}
+	go saveFile("memes/"+fn, m.Attachments[0].ProxyURL)
 	s.ChannelMessageSend(m.ChannelID, "Meme posted!")
 }
 
 func updatestatus(s *discordgo.Session) {
-	s.UpdateGameStatus(0, "بيننا مجانا 2021-2020 البنجابية الحرة")
+	s.UpdateGameStatus(0, fmt.Sprintf("%d goroutines", runtime.NumGoroutine()))
+}
+
+func runtimestats(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
+	var ms runtime.MemStats
+	runtime.ReadMemStats(&ms)
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("```ngoroutines: %d\nalloc: %v\ntotalalloc: %v\nsys: %v\nnumgc: %v\n```", runtime.NumGoroutine(), ms.Alloc, ms.TotalAlloc, ms.Sys, ms.NumGC))
 }
 
 func main() {
@@ -71,6 +83,8 @@ func main() {
 	commands = make(map[string]interface{})
 	commands["ping"] = ping
 	commands["postmeme"] = postmeme
+	commands["runtimestats"] = runtimestats
+
 	if err != nil {
 		panic(err)
 	}
@@ -78,13 +92,14 @@ func main() {
 	discord.AddHandler(messageCreate)
 
 	err = discord.Open()
+	defer discord.Close()
 	if err != nil {
 		panic(err)
 	}
 	go updatestatus(discord)
 	fmt.Println("Now running bot. Press CTRL+C to quit")
 	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
-	discord.Close()
+
 }
